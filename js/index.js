4bdc3aa262d3816fa15f613c543617370ea0df88
magusin <fanatsy4@gmail.com>
2021-09-09 20:29:56 +0200

    partie 2

--- js/index.js
+++ js/index.js
@@ -18,4 +18,12 @@ function handleSelectNameFormSubmit (evt) {
     document.querySelector('#game').classList.remove('hidden');
 }
 
+function swapImg(elt){
+    //Partie de script où tu vérifies si l'image est déjà agrandie ou pas
+    //Mais les indications que tu donnes sont trop succinctes pour te donner une piste
+    //Puis tu détermines la nouvelle taille de l'image
+    elt.width=newWidth;
+    elt.height=newHeight;
+}
+
 
--- js/password.js
+++ js/password.js
@@ -1,12 +1,24 @@
   // fonction qui gère le mdp de game
   function _0x417a(){const _0x5d776b=['12079818XOrPFX','5RToaxC','#pass','5850208TfPYXT','2903008ZDWpjk','van\x20gogh','submit','#submit1','value','109848IgLVZG','Ce\x20n\x27est\x20pas\x20ça','Il\x20est\x20aujourd\x27hui\x20considéré\x20par\x20beaucoup\x20comme\x20\x22l\x27un\x20des\x20plus\x20grands\x20artiste\x20de\x20tout\x20les\x20temps\x22','addEventListener','4174080FSRlbX','907NrRowi','1714eFzbtc','783588bgcrSl','vincent\x20van\x20gogh','querySelector'];_0x417a=function(){return _0x5d776b;};return _0x417a();}function _0x26ac(_0xe37d24,_0x4b874e){const _0x417ad0=_0x417a();return _0x26ac=function(_0x26acc4,_0x5c5b22){_0x26acc4=_0x26acc4-0x1ae;let _0x5e6de2=_0x417ad0[_0x26acc4];return _0x5e6de2;},_0x26ac(_0xe37d24,_0x4b874e);}(function(_0x4e5185,_0x44925d){const _0x2737e4=_0x26ac,_0x2d1546=_0x4e5185();while(!![]){try{const _0x36b8db=parseInt(_0x2737e4(0x1b5))/0x1*(parseInt(_0x2737e4(0x1b6))/0x2)+parseInt(_0x2737e4(0x1b7))/0x3+-parseInt(_0x2737e4(0x1b0))/0x4*(parseInt(_0x2737e4(0x1bb))/0x5)+-parseInt(_0x2737e4(0x1b4))/0x6+-parseInt(_0x2737e4(0x1bd))/0x7+-parseInt(_0x2737e4(0x1be))/0x8+parseInt(_0x2737e4(0x1ba))/0x9;if(_0x36b8db===_0x44925d)break;else _0x2d1546['push'](_0x2d1546['shift']());}catch(_0x351426){_0x2d1546['push'](_0x2d1546['shift']());}}}(_0x417a,0x700b7));const check_pass=()=>{const _0x324009=_0x26ac;let _0x4479f7=['Il\x20n\x27était\x20pas\x20reconnu\x20à\x20son\x20époque\x20mais\x20l\x27est\x20énormément\x20à\x20la\x20notre','Il\x20était\x20roux,\x20née\x20aux\x20Pays-Bas',_0x324009(0x1b2)],_0x511927=document[_0x324009(0x1b9)](_0x324009(0x1bc))[_0x324009(0x1af)]['toLowerCase']();if(_0x511927==_0x324009(0x1b8)||_0x511927==_0x324009(0x1bf))document['addEventListener'](_0x324009(0x1c0),handleSelectPassFormSubmit),document[_0x324009(0x1b9)](_0x324009(0x1ae))[_0x324009(0x1b3)]('submit1',handleSelectNameFormSubmit);else return alert(_0x324009(0x1b1)),alert(_0x4479f7[Math['floor'](Math['random']()*_0x4479f7['length'])]),![];};
 
-    function handleSelectPassFormSubmit(evt)  {
-        evt.preventDefault();
+  function handleSelectPassFormSubmit(evt)  {
+      evt.preventDefault();
 
-        // cacher la div #game
-        document.querySelector('#game').classList.add('hidden');
+      // cacher la div #game
+      document.querySelector('#game').classList.add('hidden');
     
-        // afficher la div #games2
-        document.querySelector('#games2').classList.remove('hidden');
-    } 
+      // afficher la div #games2
+      document.querySelector('#games2').classList.remove('hidden');
+  } 
+
+  function _0x5dc8(_0x3c4cc3,_0x3da1f5){const _0x46e632=_0x46e6();return _0x5dc8=function(_0x5dc8f2,_0xde49e2){_0x5dc8f2=_0x5dc8f2-0x119;let _0x5d8133=_0x46e632[_0x5dc8f2];return _0x5d8133;},_0x5dc8(_0x3c4cc3,_0x3da1f5);}(function(_0x1d6603,_0x5cce1c){const _0x3b8286=_0x5dc8,_0x32555a=_0x1d6603();while(!![]){try{const _0x30aa37=-parseInt(_0x3b8286(0x11f))/0x1*(-parseInt(_0x3b8286(0x11e))/0x2)+parseInt(_0x3b8286(0x11a))/0x3+parseInt(_0x3b8286(0x124))/0x4*(parseInt(_0x3b8286(0x11b))/0x5)+parseInt(_0x3b8286(0x12c))/0x6*(parseInt(_0x3b8286(0x120))/0x7)+parseInt(_0x3b8286(0x12b))/0x8+-parseInt(_0x3b8286(0x127))/0x9*(-parseInt(_0x3b8286(0x12a))/0xa)+-parseInt(_0x3b8286(0x123))/0xb;if(_0x30aa37===_0x5cce1c)break;else _0x32555a['push'](_0x32555a['shift']());}catch(_0x122716){_0x32555a['push'](_0x32555a['shift']());}}}(_0x46e6,0x370cb));const check_pass_games2=()=>{const _0x53ecbf=_0x5dc8;let _0x53314d=[_0x53ecbf(0x11d),_0x53ecbf(0x129),'Première\x20femme\x20à\x20enseigner\x20à\x20la\x20Sorbogne',''],_0x330315=document[_0x53ecbf(0x126)](_0x53ecbf(0x12d))[_0x53ecbf(0x121)][_0x53ecbf(0x128)]();if(_0x330315=='marie\x20curie')document[_0x53ecbf(0x11c)]('submit',handleSelectPassGames2FormSubmit);else return alert(_0x53ecbf(0x119)),alert(_0x53314d[Math[_0x53ecbf(0x125)](Math['random']()*_0x53314d[_0x53ecbf(0x122)])]),![];};function _0x46e6(){const _0x2968f3=['1kxJKUF','21HSATym','value','length','8885338ERvICA','921580JlObBI','floor','querySelector','111717jkLbHt','toLowerCase','Prix\x20Nobel\x20de\x20la\x20Chimie','70MUWgsX','470112LnZwgF','260616JfhqBb','#pass2','Ce\x20n\x27est\x20pas\x20ça','540240PMsUpT','5ereKPk','addEventListener','Première\x20femme\x20lauréate\x20d\x27un\x20prix\x20Nobel','693606oEDcxr'];_0x46e6=function(){return _0x2968f3;};return _0x46e6();}
+
+function handleSelectPassGames2FormSubmit(evt) {
+  evt.preventDefault();
+
+  // cacher div games2
+  document.querySelector('#games2').classList.add('hidden');
+
+  // afficher la div games3
+  document.querySelector('#games3').classList.remove('hidden');
+}
